365f73b48f9c9bffa46376a9cda8faf78a655a5b
reggietesting <164204139+reggietesting@users.noreply.github.com>
2025-03-07 14:11:18 +0000

    finish

--- workspace/Board.java
+++ workspace/Board.java
@@ -12,7 +12,7 @@ import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
+import java.util.Random;
 import javax.swing.*;
 
 //You will be implmenting a part of a function and a whole function in this document. Please follow the directions for the 
@@ -59,7 +59,18 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         this.addMouseMotionListener(this);
 
         //TO BE IMPLEMENTED FIRST
-     
+        
+        int counted = 0;
+
+        for (int i = 0; i < 8; i++) {
+            for (int index = 0; index < 8; index ++) {
+                board[i][index] = new Square(this, counted % 2 == 0, i, index);
+                this.add(board[i][index]); 
+                counted++;
+            }
+            counted++;
+        }
+
       //for (.....)  
 //        	populate the board with squares here. Note that the board is composed of 64 squares alternating from 
 //        	white to black.
@@ -79,10 +90,63 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 	//set up the board such that the black pieces are on one side and the white pieces are on the other.
 	//since we only have one kind of piece for now you need only set the same number of pieces on either side.
 	//it's up to you how you wish to arrange your pieces.
-    private void initializePieces() {
-    	
-    	board[0][0].put(new Piece(true, RESOURCES_WKING_PNG));
 
+    /*
+     * POST CONDITION: board initilized
+     * POST CONDITION: set up board with two white pieces, and two black pieces.
+     */
+    private void initializePieces() {
+        int columnIndex = 1;
+
+        /*ArrayList<String> pieces = new ArrayList<String>();
+        
+        pieces.add("rook");
+        pieces.add("knight");
+        pieces.add("bishop");
+        pieces.add("king");
+        pieces.add("queen");
+        pieces.add("bishop");
+        pieces.add("rook");
+        pieces.add("knight");
+        
+        for (int i = 0; i < 16; i ++) {
+            if ((i > 1) && (i % 8 == 0)) {
+                columnIndex = 6;
+            }
+
+            String currentEndPoint = (columnIndex == 6) ? "b" : "w";
+            int imageGet = (i == 0 || i == 8 || i == 16) ? 0 : i%8;
+
+            if (i == 14 || i == 6) {
+                imageGet = 1;
+            } else if (i == 15 || i == 7) {
+                imageGet = 0;
+            }
+
+            board[columnIndex][i%8].put(new Piece(columnIndex != 6, ((columnIndex == 6) ? RESOURCES_BPAWN_PNG : RESOURCES_WPAWN_PNG)));
+            board[((columnIndex==1) ? columnIndex-1 : columnIndex+1)][i%8].put(new Piece(columnIndex != 6, currentEndPoint + pieces.get(imageGet) + ".png"));
+        };*/
+
+        board[1][0].put(new Piece(true, RESOURCES_WKING_PNG));
+        board[0][0].put(new Piece(true, RESOURCES_WKING_PNG));
+        board[6][6].put(new Piece(false, RESOURCES_BKING_PNG));
+        board[7][7].put(new Piece(false, RESOURCES_BKING_PNG));
+        //pieces.clear();
+    }
+    
+    /* POST CONDITION: Valid row and valid column
+     * POST CONDITION: Returns boolean if area is/is not occupied.
+     */
+    public boolean PieceAt(int row, int column) {
+        return this.board[row][column].isOccupied();
+    }
+
+    /* POST CONDITION: Valid row and valid column
+     * POST CONDITION: Returns square of which you inputted.
+     */
+    
+    public Square GetPiece(int row, int column) {
+        return this.board[row][column];
     }
 
     public Square[][] getSquareArray() {
@@ -103,7 +167,7 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 
     @Override
     public void paintComponent(Graphics g) {
-     
+
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
                 Square sq = board[x][y];
@@ -131,6 +195,7 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         Square sq = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
 
         if (sq.isOccupied()) {
+            //System.out.println("aaaa");
             currPiece = sq.getOccupyingPiece();
             fromMoveSquare = sq;
             if (!currPiece.getColor() && whiteTurn)
@@ -147,22 +212,74 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     //use the pieces "legal move" function to determine if this move is legal, then complete it by
     //moving the new piece to it's new board location. 
     @Override
+
+    /*
+     * POST CONDITION: valid mouse event, and current piece preferably being ~= null.
+     * POST CONDITION: if valid movement/takeover move and takeover piece/move space.
+     */
+
     public void mouseReleased(MouseEvent e) {
+        if (currPiece == null) { System.out.println("CURRENT PIECE IS NULL"); return;}
+      // Square currentSquare = (Square) this.getComponentAt(new Point(currX, currY));
         Square endSquare = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
-        
+        ArrayList <Square> allowedSquares = currPiece.getLegalMoves(this, fromMoveSquare);
+        ArrayList <Square> controlledSquares = currPiece.getControlledSquares(board, fromMoveSquare);
+       // System.out.println(endSquare);
         //using currPiece
+        boolean isValid = false;
         
-       
+        for (int i = 0; i < allowedSquares.size(); i++) {
+            Square comparison = allowedSquares.get(i);
+            if (comparison.getRow() == endSquare.getRow() && comparison.getCol() == endSquare.getCol()) {
+                System.out.println("abc im valid guy");
+                isValid = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < controlledSquares.size(); i++) {
+            Square comparison = controlledSquares.get(i);
+            if (comparison.getRow() == endSquare.getRow() || comparison.getCol() == endSquare.getCol()) {
+                System.out.println("im a possible controlled piece friend.");
+                isValid = true;
+                break;
+            }
+        }
+        
+        for(Square [] row: board) {
+        	for(Square s: row) {
+        		s.setBorder(null);
+     
+        	}
+        	
+        }
+
+        Square currentAt = board[endSquare.getRow()][endSquare.getCol()];
+
+        if (isValid) {
+            fromMoveSquare.removePiece();
+            currentAt.put(currPiece);
+        }
+
+        System.out.println(isValid);
         fromMoveSquare.setDisplay(true);
         currPiece = null;
         repaint();
     }
+
 
     @Override
     public void mouseDragged(MouseEvent e) {
         currX = e.getX() - 24;
         currY = e.getY() - 24;
 
+        if(currPiece!= null) {
+        	for(Square s: currPiece.getLegalMoves(this, fromMoveSquare)) {
+        		s.setBorder(BorderFactory.createLineBorder(Color.MAGENTA));
+        	}
+        	
+        }
+        
         repaint();
     }
 
--- workspace/Piece.java
+++ workspace/Piece.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -14,7 +15,10 @@ import javax.imageio.ImageIO;
 public class Piece {
     private final boolean color;
     private BufferedImage img;
-    
+    private static Random lol = new Random();
+    private static int rowLimit = lol.nextInt(1,3);
+    private static int columnLimit = lol.nextInt(1,3);
+
     public Piece(boolean isWhite, String img_file) {
         this.color = isWhite;
         
@@ -49,8 +53,98 @@ public class Piece {
     // TO BE IMPLEMENTED!
     //return a list of every square that is "controlled" by this piece. A square is controlled
     //if the piece capture into it legally.
-    public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
-     return null;
+
+    
+    /* POST CONDITION: valid boad array and valid starting square
+     * POST CONDITION: returns pieces that you can capture at this very moment
+     */
+    public ArrayList<Square> getControlledSquares(Square[][] b, Square start) {
+      ArrayList<Square> controlledSquares = new ArrayList<Square>();
+
+      int myColumn = start.getCol();
+      int myRow = start.getRow();
+
+      for (int i = myRow; i < 8; i++) {
+        if (i == myRow) {
+          continue;
+        }
+
+        int Distance = Math.abs(i-myRow);
+
+        if (Distance > rowLimit) {
+          System.out.println("toofar");
+          break;
+        }
+
+        if (b[i][myColumn].isOccupied() && b[i][myColumn].getColor() == color) {
+          controlledSquares.add(b[i][myColumn]);
+          System.out.println("controlled");
+        }
+      }
+
+      for (int i = myRow; i <= 0; i--) {
+        if (i < 0) {
+          break; // bro??
+        }
+
+        if (i == myRow) {
+          continue;
+        }
+        int Distance = Math.abs(i-myRow);
+
+        if (Distance > rowLimit) {
+          break;
+        }
+
+        if (b[i][myColumn].isOccupied() && b[i][myColumn].getColor() == color) {
+          controlledSquares.add(b[i][myColumn]);
+          System.out.println("controlled");
+          System.out.println("ROW: "+ i);
+          System.out.println("COLUMN: " +myColumn);
+        }
+      }
+
+      for (int i = myColumn; i > 8; i++) {
+        int Distance = Math.abs(i-myColumn);
+
+        if (i == myColumn) {
+          continue;
+        }
+
+        if (Distance > columnLimit) {
+          break;
+        }
+
+        if (b[myRow][i].isOccupied() && b[myRow][i].getColor() == color) {
+          controlledSquares.add(b[myRow][i]);
+          System.out.println("controlled");
+        }
+      }
+
+
+      for (int i = myColumn; i >= 0; i--) {
+        if (i < 0) {
+          break;
+        }
+
+        if (i == myColumn) {
+          continue;
+        }
+
+        int Distance = Math.abs(i-myColumn);
+
+        if (Distance > columnLimit) {
+          break;
+        }
+
+        if (b[myRow][i].isOccupied() && b[myRow][i].getColor() == color) {
+          controlledSquares.add(b[myRow][i]);
+          System.out.println("controlled");
+        }
+      }
+
+      System.out.println(controlledSquares);
+      return controlledSquares;
     }
     
 
@@ -60,7 +154,110 @@ public class Piece {
     //returns an arraylist of squares which are legal to move to
     //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
     //going to score any points.
+
+      /* POST CONDITION: valid boad and valid starting square
+     * POST CONDITION: returns places you can move at this very moment.
+     */
     public ArrayList<Square> getLegalMoves(Board b, Square start){
-    	return null;
+      // the random
+      // it can move random amounts of spaces ahead
+      // depends on how its feeling;
+
+      ArrayList<Square> allowedMovements = new ArrayList<Square>();
+
+      System.out.println("ROW:" + String.valueOf(start.getRow()));
+      System.out.println("COLUMN: "+ String.valueOf(start.getCol()));
+
+      int currentColumn = start.getCol();
+      int currentRow = start.getRow();
+
+      for (int i = currentRow; i < 8; i++) {
+        if (i == currentRow) {
+          System.out.println("ROW INCREASING: continued same index");
+          continue;
+        }
+
+        if (b.getSquareArray()[i][currentColumn].isOccupied()) {
+          System.out.println("ROW INCREASING: broken occupied");
+          break;
+        }
+
+        int Distance = Math.abs(i-currentRow); 
+
+        if (Distance > rowLimit) {
+          System.out.println("ROW INCREASING: broken distance too much row");
+          break;
+        }
+
+        allowedMovements.add(b.getSquareArray()[i][currentColumn]);
+      }
+
+      for (int i = currentRow; i >= 0; i--) {
+        if (i == currentRow) {
+          System.out.println("ROW DECREASING: continued same row");
+          continue;
+        }
+
+        if (b.getSquareArray()[i][currentColumn].isOccupied()) {
+          System.out.println("ROW DECREASING: broken occupied row");
+          break;
+        }
+
+        int Distance = Math.abs(i-currentRow); 
+
+        if (Distance > rowLimit) {
+          System.out.println("ROW DECREASING: broken distance too much row");
+          break;
+        }
+
+        allowedMovements.add(b.getSquareArray()[i][currentColumn]);
+      }
+
+      for (int i = currentColumn; i < 8; i++) {
+        if (i == currentColumn) {
+          System.out.println("COLUMN INCREASING: continued same column");
+          continue;
+        }
+
+        if (b.getSquareArray()[currentRow][i].isOccupied()) {
+          System.out.println("COLUMN INCREASING: broken occupied");
+          break;
+        }
+
+        int Distance = Math.abs(i-currentColumn); 
+
+        if (Distance > columnLimit) {
+          System.out.println("COLUMN INCREASING: broken distance");
+          break;
+        }
+
+        allowedMovements.add(b.getSquareArray()[currentRow][i]);
+      }
+
+      for (int i = currentColumn; i >= 0; i--) {
+        if (i == currentColumn) {
+          System.out.println("COLUMN DECREASING: continued same column");
+          continue;
+        }
+
+        if (b.getSquareArray()[currentRow][i].isOccupied()) {
+          System.out.println("COLUMN DECREASING: broken cuz occupied");
+          break;
+        }
+
+        int Distance = Math.abs(i-currentColumn); 
+
+        if (Distance > columnLimit) {
+          System.out.println("COLUMN DECREASING: broken cuz distance >");
+          break;
+        }
+
+        allowedMovements.add(b.getSquareArray()[currentRow][i]);
+      }
+
+
+
+      System.out.println(allowedMovements);
+      return allowedMovements;
     }
 }
